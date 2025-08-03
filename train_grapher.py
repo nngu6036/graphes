dc2c1029c1c852375bc195b5c9537ab6356766c9
Quang Nguyen <technicalmanager@gmail.com>
2025-08-03 20:09:24 +1000

    Update generate

--- train_grapher.py
+++ train_grapher.py
@@ -135,7 +135,7 @@ def main(args):
             sample_graphs = random.sample(train_graphs,min(len(train_graphs),config['inference']['generate_samples']))
             degree_sequences = [[deg for _, deg in graph.degree()] for graph in sample_graphs]
             generated_graphs = model.generate_with_configuration_model(config['inference']['generate_samples'],T,degree_sequences = degree_sequences, msvae_model = None)
-             print(f"Evaluate generated graphs sampled from training")
+            print(f"Evaluate generated graphs sampled from training")
             print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
             print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
             print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
