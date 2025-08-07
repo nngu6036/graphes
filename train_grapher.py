c37321a49fe4df1932ac03b976244717fed83a97
Quang Nguyen <technicalmanager@gmail.com>
2025-08-08 08:04:22 +1000

    Refactor

--- train_grapher.py
+++ train_grapher.py
@@ -147,69 +147,67 @@ def main(args):
         print(f"Model saved to {args.output_model}")
     if args.evaluate:
         graph_eval = GraphsEvaluator()
-        if args.ablation:
-            sample_graphs = random.choices(train_graphs,k=config['inference']['generate_samples'])
-            test_seqs = [[deg for _, deg in graph.degree()] for graph in test_graphs ]
-            degree_sequences = [[deg for _, deg in graph.degree()] for graph in sample_graphs]
+        deg_eval = DegreeSequenceEvaluator()
+        sample_graphs = random.choices(train_graphs,k=config['inference']['generate_samples'])
+        test_seqs = [[deg for _, deg in graph.degree()] for graph in test_graphs ]
+        degree_sequences = [[deg for _, deg in graph.degree()] for graph in sample_graphs]
 
-            """
-            generated_graphs, generated_seqs = model.generate_without_msvae(T,degree_sequences,k_eigen,method = 'constraint_configuration_model')
-            print(f"Evaluate generated graphs sampled from training using constraint configuration model")
-            print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
-            print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
-            print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
-            deg_eval = DegreeSequenceEvaluator()
-            print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
-            print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
+        """
+        generated_graphs, generated_seqs = model.generate_without_msvae(T,degree_sequences,k_eigen,method = 'constraint_configuration_model')
+        print(f"Evaluate generated graphs sampled from training using constraint configuration model")
+        print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
+        print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
+        print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
+        deg_eval = DegreeSequenceEvaluator()
+        print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
+        print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
 
-            generated_graphs, generated_seqs = model.generate_without_msvae(T,degree_sequences,k_eigen,method ='configuration_model')
-            print(f"Evaluate generated graphs sampled from training using configuraiton model")
-            print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
-            print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
-            print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
-            deg_eval = DegreeSequenceEvaluator()
-            print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
-            print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
-            """
-            generated_graphs, generated_seqs = model.generate_without_msvae(T,degree_sequences,k_eigen,method = 'havei_hakimi')
-            print(f"Evaluate generated graphs sampled from training using havei-hakimi model")
-            print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
-            print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
-            print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
-            deg_eval = DegreeSequenceEvaluator()
-            print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
-            print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
-        else:
-            """
-            generated_graphs, generated_seqs = model.generate(config['inference']['generate_samples'],T, msvae_model,k_eigen,method = 'constraint_configuration_model')
-            print(f"Evaluate generated graphs using constraint Configuraiton Model")
-            print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
-            print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
-            print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
-            deg_eval = DegreeSequenceEvaluator()
-            test_seqs = [[deg for _, deg in graph.degree()] for graph in test_graphs ]
-            print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
-            print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
+        generated_graphs, generated_seqs = model.generate_without_msvae(T,degree_sequences,k_eigen,method ='configuration_model')
+        print(f"Evaluate generated graphs sampled from training using configuraiton model")
+        print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
+        print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
+        print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
+        deg_eval = DegreeSequenceEvaluator()
+        print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
+        print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
+        """
+        generated_graphs, generated_seqs = model.generate_without_msvae(T,degree_sequences,k_eigen,method = 'havei_hakimi')
+        print(f"Evaluate generated graphs sampled from training using havei-hakimi model")
+        print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
+        print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
+        print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
+        
+        print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
+        print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
+        """
+        generated_graphs, generated_seqs = model.generate(config['inference']['generate_samples'],T, msvae_model,k_eigen,method = 'constraint_configuration_model')
+        print(f"Evaluate generated graphs using constraint Configuraiton Model")
+        print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
+        print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
+        print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
+        deg_eval = DegreeSequenceEvaluator()
+        test_seqs = [[deg for _, deg in graph.degree()] for graph in test_graphs ]
+        print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
+        print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
 
-            generated_graphs, generated_seqs = model.generate(config['inference']['generate_samples'],T, msvae_model,k_eigen,method = 'configuration_model')
-            print(f"Evaluate generated graphs using  Configuraiton Model")
-            print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
-            print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
-            print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
-            deg_eval = DegreeSequenceEvaluator()
-            test_seqs = [[deg for _, deg in graph.degree()] for graph in test_graphs ]
-            print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
-            print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
-            """
-            generated_graphs, generated_seqs = model.generate(config['inference']['generate_samples'],T, msvae_model,k_eigen,method = 'havei_hakimi')
-            print(f"Evaluate generated graphs using Havei Hamimi Model")
-            print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
-            print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
-            print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
-            deg_eval = DegreeSequenceEvaluator()
-            test_seqs = [[deg for _, deg in graph.degree()] for graph in test_graphs ]
-            print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
-            print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
+        generated_graphs, generated_seqs = model.generate(config['inference']['generate_samples'],T, msvae_model,k_eigen,method = 'configuration_model')
+        print(f"Evaluate generated graphs using  Configuraiton Model")
+        print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
+        print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
+        print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
+        deg_eval = DegreeSequenceEvaluator()
+        test_seqs = [[deg for _, deg in graph.degree()] for graph in test_graphs ]
+        print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
+        print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
+        """
+        generated_graphs, generated_seqs = model.generate(config['inference']['generate_samples'],T, msvae_model,k_eigen,method = 'havei_hakimi')
+        print(f"Evaluate generated graphs using Havei Hamimi Model and MS-VAE")
+        print(f"MMD Degree: {graph_eval.compute_mmd_degree_emd(test_graphs,generated_graphs,max_node)}")
+        print(f"MMD Clustering Coefficient: {graph_eval.compute_mmd_cluster(test_graphs,generated_graphs)}")
+        print(f"MMD Orbit count: {graph_eval.compute_mmd_orbit(test_graphs,generated_graphs)}")
+
+        print(f"KL Distance: {deg_eval.evaluate_multisets_kl_distance(test_seqs,generated_seqs,max_node)}")
+        print(f"MMD Distance: {deg_eval.evaluate_multisets_mmd_distance(test_seqs,generated_seqs,max_node)}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='GRAPH-ER Model')
@@ -220,6 +218,5 @@ if __name__ == "__main__":
     parser.add_argument('--output-model', type=str, help='Path to save the trained model')
     parser.add_argument('--input-model', type=str, help='Path to load a pre-trained model')
     parser.add_argument('--evaluate', action='store_true', help='Whether we evaluate the model')
-    parser.add_argument('--ablation', action='store_true', help='Whether to run ablation study')
     args = parser.parse_args()
     main(args)
